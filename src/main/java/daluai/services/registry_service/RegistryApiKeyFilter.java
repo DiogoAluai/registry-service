ccaed7320a5741313a5074b4a7b226b2165023bc
Diogo Aluai <diogoaluai@gmail.com>
2024-09-06 01:54:04 +0100

    Add API header encryption

--- src/main/java/daluai/services/registry_service/RegistryApiKeyFilter.java
+++ src/main/java/daluai/services/registry_service/RegistryApiKeyFilter.java
@@ -1,15 +1,21 @@
 package daluai.services.registry_service;
 
-import daluai.lib.network_utils.property.PropertyManager;
-import daluai.lib.services_parent.ApiKeyFilter;
+import daluai.lib.network_utils.property.PropertyKeys;
+import daluai.lib.services_parent.EncryptedApiKeyFilter;
 import org.springframework.stereotype.Component;
 
-import static daluai.lib.network_utils.property.PropertyKeys.API_KEY;
+import static daluai.lib.network_utils.property.PropertyKeys.REGISTRY_API_KEY_SHA512_DIGEST;
+import static daluai.lib.registry_api.Coms.REGISTRY_PROPERTY_MANAGER;
 
 @Component
-public class RegistryApiKeyFilter extends ApiKeyFilter {
+public class RegistryApiKeyFilter extends EncryptedApiKeyFilter {
     @Override
-    protected String getApiKey() {
-        return PropertyManager.DEFAULT_INSTANCE.getProperty(API_KEY);
+    protected String getAESSecret() {
+        return REGISTRY_PROPERTY_MANAGER.getProperty(PropertyKeys.REGISTRY_API_KEY_AES_SECRET);
+    }
+
+    @Override
+    protected String getHashedApiKey() {
+        return REGISTRY_PROPERTY_MANAGER.getProperty(REGISTRY_API_KEY_SHA512_DIGEST);
     }
 }
